431635f2d0e8a07c6d06aa4f893751df3e4d29d9
Sebastian Lukas <sebastian.lukas@hotmail.com>
2024-01-12 22:31:25 +0100

    Adds dictionary

---
+++ src/Dictionary.spec.ts
@@ -0,0 +1,36 @@
+import {Dictionary} from "./Dictionary";
+
+describe('Dictionary', () => {
+
+
+    it('should read words from file', () => {
+        const dictionary = new Dictionary();
+        const words = dictionary.readFromFile('src/words/words');
+        expect(words.length).toBeGreaterThan(0);
+    });
+
+    it('should set and get words', () => {
+        const dictionary = new Dictionary();
+        dictionary.setWords( ['TEST', 'testen']);
+        expect(dictionary.getWords()).toEqual(['TEST', 'TESTEN']);
+    });
+
+    it('should remove words from block list', () => {
+        const dictionary = new Dictionary();
+        const words = ['test', 'testen'];
+        dictionary.setWords(words);
+        dictionary.removeWords(['testen']);
+        expect(dictionary.getWords()).toEqual(['TEST']);
+    });
+
+    it('should only includes words with letters A,B,C', () => {
+        const dictionary = new Dictionary();
+        const words = ['Apfel', 'Bananen', 'Cranberry', 'Blocked'];
+        const filtered = dictionary
+            .setWords(words)
+            .filterLetters(['A', 'B'])
+            .removeWords(['Blocked'])
+            .getWords();
+        expect(dictionary.getWords()).toEqual(['APFEL', 'BANANEN']);
+    });
+});
---
+++ src/Dictionary.ts
@@ -0,0 +1,35 @@
+import * as fs from "fs";
+import {Letter} from "./Dice";
+
+export class Dictionary {
+
+    private words: string[] = [];
+
+    readFromFile(path: string): string[] {
+        return fs.readFileSync(path, 'utf8').split('\n');
+    }
+
+    getWords(): string[] {
+        return this.words;
+    }
+
+    setWords(words: string[]): Dictionary {
+        this.words = words.map(word => word.toUpperCase());
+        return this;
+    }
+
+
+    removeWords(blockListWords: string[]): Dictionary {
+        const uppercaseBlockListWords = blockListWords.map(word => word.toUpperCase());
+        this.words = this.words.filter(word => !uppercaseBlockListWords.includes(word));
+        return this;
+    }
+
+    filterLetters(letters: Letter[]): Dictionary {
+        this.words = this.words.filter(word => {
+           const letter = word.charAt(0).toUpperCase() as Letter;
+              return letters.includes(letter);
+        });
+        return this;
+    }
+}
